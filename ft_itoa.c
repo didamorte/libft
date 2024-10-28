45faa7fa70a7d20ee93dc5abd32e696a210f45e4
Diogo Ribeiro Magalhaes <diogribe@c1r8s1.42porto.com>
2024-10-28 17:04:25 +0000

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,60 @@
 /*   By: diogribe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:26:42 by diogribe          #+#    #+#             */
-/*   Updated: 2024/10/28 12:27:20 by diogribe         ###   ########.fr       */
+/*   Updated: 2024/10/28 17:04:04 by diogribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_numcount(int nb)
+{
+	int	c;
+
+	c = 0;
+	if (nb <= 0)
+		c++;
+	while (nb)
+	{
+		nb /= 10;
+		c++;
+	}
+	return (c);
+}
+
+static void	ft_revnbr(int n, int len, int i, char *str)
+{
+	while (i < len)
+	{
+		str[len - 1] = n % 10 + '0';
+		n /= 10;
+		len--;
+	}
+}
+
 char	*ft_itoa(int n)
 {
-	
+	char	*str;
+	int		len;
+	int		i;
+
+	len = ft_numcount(n);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	if (n == -2147483648)
+	{
+		str[i++] = '-';
+		str[i++] = '2';
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		str[i++] = '-';
+		n *= -1;
+	}
+	ft_revnbr(n, len, i, str);
+	str[len] = '\0';
+	return (str);
 }
